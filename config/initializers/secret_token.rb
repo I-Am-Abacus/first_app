196b17a05c0553e2df4154833ce0263410c3dd66
I Am Abacus <martinrpayneuk+git@gmail.com>
2014-03-27 11:37:31 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '36280021709e449a480a4025497e0549b12c6e7309d83e67c892167f990c5f5fffc64f8cb1c650faff1f1abd2a8e9fcd5126e7b31ba2b64fcb030cfa2ae54965'
